3c1be48c207388f8c6c2f621facf5bc5ac0803ae
mohit singh <mk8652195@gmail.com>
2024-12-09 15:46:50 +0530

    first changes

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const  toggleMode = ()=>{
+       if(mode === 'light'){
+       setMode('dark');
+       document.body.style.backgroundColor = '#042743';
+       showAlert("Dark mode has been enabled", "success");
+       }else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled", "success");
+       }
+        }
+
+  return (
+  <>
+   <Navbar title="TextUtils" abouttext="about TextUtils" mode={mode}  toggleMode={toggleMode} />
+  <Alert alert={alert}/>
+    <div className="container my-3">
+       <Textform showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+  </div>
+  </>
+  );
+}
+
+export default App;
